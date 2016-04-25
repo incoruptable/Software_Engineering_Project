bf3da3ce6773ab000cb13ceb40c907c632110a38
incoruptable <zachbrandt51@yahoo.com>
2016-04-24 22:13:38 -0500

    j

---
+++ CSCEProject/Store.java
@@ -0,0 +1,33 @@
+
+public class Store {
+	private int storeID;
+	private String storeName;
+	private String storeAddress;
+	private String storePhone;
+	public int getStoreID() {
+		return storeID;
+	}
+	public void setStoreID(int storeID) {
+		this.storeID = storeID;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreAddress() {
+		return storeAddress;
+	}
+	public void setStoreAddress(String storeAddress) {
+		this.storeAddress = storeAddress;
+	}
+	public String getStorePhone() {
+		return storePhone;
+	}
+	public void setStorePhone(String storePhone) {
+		this.storePhone = storePhone;
+	}
+	
+	
+}
